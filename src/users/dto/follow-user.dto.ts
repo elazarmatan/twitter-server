685cab8ae146ec36dab878d4008fea82b4a92e7e
elazar matan <elazarmata8@gmail.com>
2026-03-09 21:32:22 +0200

    fix

--- src/users/dto/follow-user.dto.ts
+++ src/users/dto/follow-user.dto.ts
@@ -1,4 +1,8 @@
+import { IsString } from "class-validator";
+
 export class FollowUserDto {
+   @IsString()
+  currentUser: string; // the user performing the action (optional, can come from JWT)
+   @IsString()
   username: string; // the user to follow
-  currentUser?: string; // the user performing the action (optional, can come from JWT)
 }
